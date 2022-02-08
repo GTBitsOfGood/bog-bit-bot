1e73e2fb6a199b4ee839e82149ed790f9a2da7e4
sukhmai <sukhmai.kapur@gmail.com>
2022-02-08 13:23:26 -0500

    Bot replies in threads. Added teams command.

--- app.js
+++ app.js
@@ -39,6 +39,16 @@ const findOrCreateUser = async (userId) => {
   return user;
 }
 
+const botReply = async (text, message, say1) => {
+  if (message.channel_type == 'im') {
+    await say1(text); 
+  } else if (message.thread_ts) {
+    await say1({ text: text, thread_ts: message.thread_ts}); 
+  } else {
+    await say1({ text: text, thread_ts: message.ts});
+  }
+}
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -52,20 +62,21 @@ app.message('help', async ({ message, say }) => {
     if (messageArray[0] != "help") return;
     
     if (messageArray.length > 1) return;
-    await say("```Available commands:\n" 
+    await botReply("```Available commands:\n" 
     + "help [role]                   See this command. Optional role param for extra commands.\n" 
     + "checkin {password}            Attendance at General/Team Meetings + office hours!\n" 
     + "bits                          See your current bit count.\n"
     + "profile                       See your current profile.\n" 
-    + "set team                      Set your current team.\n" 
-    + "leaderboard [amt]             See the current bit leaderboard. Optional param for length. \n```" );
+    + "set team                      Set your current team.\n"
+    + "teams                         See the current list of teams.\n"
+    + "leaderboard [amt]             See the current bit leaderboard. Optional param for length. \n```" , message, say);
   });
 
 app.message('help exec', async ({ message, say }) => {
   const messageArray = message.text.split(' ');
   if (messageArray[0] != "help" && messageArray[1] != "exec") return;
 
-  await say("```Available commands:\n" 
+  await botReply("```Available commands:\n" 
   + "help [role]                     See this command. Optional role param for extra commands.\n" 
   + "set password {password}         Set attendance password.\n"
   + "set form {form_link}            Set attendance form link.\n" 
@@ -74,7 +85,7 @@ app.message('help exec', async ({ message, say }) => {
   + "remove teams {team1, team2...}  Add teams to the team list.\n"
   + "set role {user} {role}          Set a user's role. Options are exec, leader, member.\n"
   + "give bits {user, channel} {amt} Give a user bits. Use @username or @channel.\n"
-  + "give team_bits {team} {amt}     Give a team bits. Check team list with set team command.\n```");
+  + "give team_bits {team} {amt}     Give a team bits. Check team list with set team command.\n```", message, say);
 });
   
 app.message('bits', async ({ message, say }) => {
@@ -82,7 +93,21 @@ app.message('bits', async ({ message, say }) => {
   if (messageArray[0] != "bits") return;
   
   const user = await findOrCreateUser(message.user);
-  await say(`You have ${user.bits} bits!`);
+  
+  await botReply(`You have ${user.bits} bits!`, message, say);
+});
+
+app.message('teams', async ({ message, say }) => {
+  const messageArray = message.text.split(' ');
+  if (messageArray[0] != "teams") return;
+  
+  let teamConfig = await config.findOne({ teams: { $exists: true }});
+  if (!teamConfig || !teamConfig.teams || !teamConfig.teams.length) {
+    return await botReply("No teams set.", message, say) 
+  }
+  let teams = teamConfig.teams;
+  
+  await botReply(`The current teams are: ${teams.join(', ')}.`, message, say);
 });
 
 app.message('profile', async ({ message, say }) => {
@@ -90,7 +115,7 @@ app.message('profile', async ({ message, say }) => {
   if (messageArray[0] != "profile") return;
   
   const user = await findOrCreateUser(message.user);
-  await say(`> Username: <@${user.userId}> \n` + `> Role: ${user.role} \n` + `> Bits: ${user.bits} \n` +`> Team: ${user.team} `);
+  await botReply(`> Username: <@${user.userId}> \n` + `> Role: ${user.role} \n` + `> Bits: ${user.bits} \n` +`> Team: ${user.team} `, message, say);
 });
 
 app.message('set team', async ({ message, say }) => {
@@ -100,7 +125,10 @@ app.message('set team', async ({ message, say }) => {
   const user = await findOrCreateUser(message.user);
   let teamOptions = [];
   let teamConfig = await config.findOne({ teams: { $exists: true }});
-  let teams = teamConfig.teams ? teamConfig.teams : []; 
+  if (!teamConfig || !teamConfig.teams || !teamConfig.teams.length) {
+    return await botReply("No teams set.", message, say) 
+  }
+  let teams = teamConfig.teams;
   for (let i = 0; i < teams.length; i++) {
     teamOptions.push({
       "text": {
@@ -137,14 +165,14 @@ app.message('checkin', async({ message, say }) => {
   if (messageArray[0] != "checkin") return;
 
   if (messageArray.length < 2) {
-    return await say('Please provide a password.');
+    return await botReply('Please provide a password.', message, say);
   }
   const passConfig = await config.findOne({ password: { $exists: true }});
   if (await bcrypt.compare(messageArray[1], passConfig.password)) {
     const user = await findOrCreateUser(message.user);
     const formConfig = await config.findOne({ form: { $exists: true }});
     if (user.password == passConfig.password) {
-      return await say('Already checked in.');
+      return await botReply('Already checked in.', message, say);
     }
     const updateDoc = {
       $set: {
@@ -153,12 +181,12 @@ app.message('checkin', async({ message, say }) => {
       }
     };
     await users.updateOne({ userId: user.userId}, updateDoc);
-    await say(`Checked in. You now have ${user.bits + 1} bits!`);
+    await botReply(`Checked in. You now have ${user.bits + 1} bits!`, message, say);
     if (formConfig) {
-      await say(`Please visit ${formConfig.form} to fill out today's form.`);
+      await botReply(`Please visit ${formConfig.form} to fill out today's form.`, message, say);
     }
   } else {
-    await say(`Incorrect password.`);
+    await botReply(`Incorrect password.`);
   }  
 })
 
@@ -168,11 +196,11 @@ app.message('set password', async ({ message, say }) => {
 
   const user = await findOrCreateUser(message.user);
   if (user.role != "exec") {
-    return await say('Insufficient Permissions. Contact exec if you need access.');
+    return await botReply('Insufficient Permissions. Contact exec if you need access.', message, say);
   }
   
   if (messageArray.length < 3) {
-    return await say('Please provide a password');
+    return await botReply('Please provide a password', message, say);
   }
   const newPass = messageArray[2];
   const hashedPassword = await bcrypt.hash(newPass, 12);
@@ -182,7 +210,7 @@ app.message('set password', async ({ message, say }) => {
     }
   };
   await config.updateOne({ password: { $exists: true }}, updateDoc, { upsert: true });
-  await say(`Updated password.`);
+  await botReply(`Updated password.`, message, say);
 });
 
 app.message('add teams', async ({ message, say }) => {
@@ -191,10 +219,10 @@ app.message('add teams', async ({ message, say }) => {
   
   const user = await findOrCreateUser(message.user);
   if (user.role != "exec") {
-    return await say('Insufficient Permissions. Contact exec if you need access.');
+    return await botReply('Insufficient Permissions. Contact exec if you need access.', message, say);
   }
   if (messageArray.length < 3) {
-    return await say('Please provide at least one team.');
+    return await botReply('Please provide at least one team.', message, say);
   }
   
   let teamConfig = await config.findOne({ teams: { $exists: true }});
@@ -208,7 +236,7 @@ app.message('add teams', async ({ message, say }) => {
     }
   };
   await config.updateOne({ teams: { $exists: true }}, updateDoc, { upsert: true });
-  await say(`Added teams.`);
+  await botReply(`Added teams.`, message, say);
 });
 
 app.message('remove teams', async ({ message, say }) => {
@@ -217,24 +245,24 @@ app.message('remove teams', async ({ message, say }) => {
 
   const user = await findOrCreateUser(message.user);
   if (user.role != "exec") {
-    return await say('Insufficient Permissions. Contact exec if you need access.');
+    return await botReply('Insufficient Permissions. Contact exec if you need access.', message, say);
   }
   if (messageArray.length < 3) {
-    return await say('Please provide at least one team.');
+    return await botReply('Please provide at least one team.', message, say);
   }
   
   let teamConfig = await config.findOne({ teams: { $exists: true }});
   let teams = !teamConfig || !teamConfig.teams ? [] : teamConfig.teams;
   for (let i = 2; i < messageArray.length; i++) {
-    teams.splice(teams.indexOf(messageArray[i]));
+    teams.splice(teams.indexOf(messageArray[i]), 1);
   }
   const updateDoc = {
     $set: {
       teams: teams
     }
   };
-  await config.updateOne({ teams: { $exists: true }}, updateDoc, { upsert: true });
-  await say(`Removed teams.`);
+  await config.updateOne({ teams: { $exists: true }}, updateDoc);
+  await botReply(`Removed teams.`, message, say);
 });
 
 app.message('set role', async({ message, say }) => {
@@ -243,23 +271,23 @@ app.message('set role', async({ message, say }) => {
 
   const user = await findOrCreateUser(message.user);
   if (user.role != "exec") {
-    return await say('Insufficient Permissions. Contact exec if you need access.');
+    return await botReply('Insufficient Permissions. Contact exec if you need access.', message, say);
   }
   const messageArray = message.text.split('@');  
   if (messageArray.length < 2) {
-    return await say('Please provide a user. Remember to use @username.');
+    return await botReply('Please provide a user. Remember to use @username.', message, say);
   }
   
   const params = messageArray[1].split('>');
   if (params.length < 2) {
-    return await say('Please provide a role. Available roles are exec, leader, member');
+    return await botReply('Please provide a role. Available roles are exec, leader, member', message, say);
   }
   const newRoleUser = params[0];
   await findOrCreateUser(newRoleUser);
   
   const newRole = params[1].substring(1).toLowerCase();
   if (newRole != "exec" && newRole != "leader" && newRole != "member") {
-    return await say('Incorrect role. Available roles are exec, leader, member.'); 
+    return await botReply('Incorrect role. Available roles are exec, leader, member.', message, say); 
   }
   const updateDoc = {
     $set: {
@@ -267,7 +295,7 @@ app.message('set role', async({ message, say }) => {
     }
   };
   await users.updateOne({ userId: newRoleUser }, updateDoc);
-  await say(`Updated <@${newRoleUser}>\'s role.`);
+  await botReply(`Updated <@${newRoleUser}>\'s role.`, message, say);
 });
 
 app.message('give bits', async({ message, client, say }) => {
@@ -276,16 +304,16 @@ app.message('give bits', async({ message, client, say }) => {
 
   const user = await findOrCreateUser(message.user);
   if (user.role != "exec") {
-    return await say('Insufficient Permissions. Contact exec if you need access.');
+    return await botReply('Insufficient Permissions. Contact exec if you need access.', message, say);
   }
   const messageArray = message.text.split(/[@!]+/);
   if (messageArray.length < 2) {
-    return await say('Please provide a user. Remember to use @username.');
+    return await botReply('Please provide a user. Remember to use @username.', message, say);
   }
 
   const params = messageArray[messageArray.length - 1].split('>');
   if (params.length < 2) {
-    return await say('Please provide the amount of bits.');
+    return await botReply('Please provide the amount of bits.', message, say);
   }
   let members = [];
   for (let i = 1; i < messageArray.length; i++) {
@@ -295,7 +323,7 @@ app.message('give bits', async({ message, client, say }) => {
   
   const bitAmount = parseInt(params[1].substring(1));
   if (!bitAmount) {
-    return await say('Please provide an integer amount.'); 
+    return await botReply('Please provide an integer amount.', message, say); 
   }
   const updateDoc = {
     $inc: {
@@ -312,7 +340,7 @@ app.message('give bits', async({ message, client, say }) => {
     await findOrCreateUser(members[i]);
   }
   await users.updateMany({ userId: {$in: members}}, updateDoc);
-  await say(`Updated bits.`);
+  await botReply(`Updated bits!`, message, say);
 });
 
 app.message('give team_bits', async({ message, say }) => {
@@ -321,23 +349,23 @@ app.message('give team_bits', async({ message, say }) => {
 
   const user = await findOrCreateUser(message.user);
   if (user.role != "exec") {
-    return await say('Insufficient Permissions. Contact exec if you need access.');
+    return await botReply('Insufficient Permissions. Contact exec if you need access.', message, say);
   }
   
   if (messageArray.length < 3) {
-    return await say('Please provide a team. Check the team list through \`set team\`.');
+    return await botReply('Please provide a team. Check the team list through \`set team\`.', message, say);
   } else if (messageArray.length < 4) {
-    return await say('Please provide the amount of bits.');
+    return await botReply('Please provide the amount of bits.', message, say);
   }
   const team = messageArray[2];
   let teamConfig = await config.findOne({ teams: { $exists: true }});
   if (!teamConfig ||!teamConfig.teams || !teamConfig.teams.includes(team)) {
-    return await say('Not a valid team.');
+    return await botReply('Not a valid team.', message, say);
   }  
   const bitAmount = parseInt(messageArray[3]);
 
   if (!bitAmount) {
-    return await say('Please provide an integer amount.'); 
+    return await botReply('Please provide an integer amount.', message, say); 
   }
   const updateDoc = {
     $inc: {
@@ -348,7 +376,7 @@ app.message('give team_bits', async({ message, say }) => {
     await findOrCreateUser(team[i]);
   }
   await users.updateMany({ team: team }, updateDoc);
-  await say(`Updated team ${team}\'s bits.`);
+  await botReply(`Updated team ${team}\'s bits.`, message, say);
 });
 
 app.message('set form', async ({ message, say }) => {
@@ -357,11 +385,11 @@ app.message('set form', async ({ message, say }) => {
 
   const user = await findOrCreateUser(message.user);
   if (user.role != "exec") {
-    return await say('Insufficient Permissions. Contact exec if you need access.');
+    return await botReply('Insufficient Permissions. Contact exec if you need access.', message, say);
   }
 
   if (messageArray.length < 3) {
-    return await say('Please provide a url to the form');
+    return await botReply('Please provide a url to the form', message, say);
   }
   const newForm = messageArray[2];
   const updateDoc = {
@@ -370,7 +398,7 @@ app.message('set form', async ({ message, say }) => {
     }
   };
   await config.updateOne({ form: { $exists: true }}, updateDoc, { upsert: true });
-  await say(`Updated form.`);
+  await botReply(`Updated form.`, message, say);
 });
 
 app.message('remove form', async ({ message, say }) => {
@@ -379,10 +407,10 @@ app.message('remove form', async ({ message, say }) => {
 
   const user = await findOrCreateUser(message.user);
   if (user.role != "exec") {
-    return await say('Insufficient Permissions. Contact exec if you need access.');
+    return await botReply('Insufficient Permissions. Contact exec if you need access.', message, say);
   }
   await config.deleteOne({ form: { $exists: true }});
-  await say(`Removed form.`);
+  await botReply(`Removed form.`, message, say);
 });
 
 app.message('leaderboard', async({ message, say }) => {
@@ -394,7 +422,7 @@ app.message('leaderboard', async({ message, say }) => {
     if (parseInt(messageArray[1])) {
       limit = parseInt(messageArray[1])
     } else {
-      return await say(`Provide an integer limit.`);
+      return await botReply(`Provide an integer limit.`, message, say);
     }
     
   }
@@ -418,7 +446,7 @@ app.action('change_team', async ({ body, ack, say }) => {
     }
   };
   await users.updateOne({ userId: userId }, updateDoc);
-  await say('Team changed!');
+  await botReply('Team changed!', message, say);
 });
 
 (async () => {
